33942383cb381b29ae5858ccc907226898caab48
Quepas <oneinchman.dev@gmail.com>
2014-02-14 15:22:01 +0100

    Adding player stand/walk animiation.

--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
-require("src/RC_Player")
+require("src.RC_Player")
 
 function love.load()
   love.window.setMode(600, 400, {vsync=true, centered=true})
   love.graphics.setBackgroundColor(0, 102, 102)
-  player = newPlayer(100, 200)
+  player = newPlayer(0, 0)
 end
 
 function love.update(dt)
   player:update(dt)
+
+  if love.keyboard.isDown('space') then
+    player:jump()
+  end
 
   if love.keyboard.isDown('left') then
     player:move(-50*dt, 0)
--- src/RC_Player.lua
+++ src/RC_Player.lua
@@ -1,40 +1,59 @@
-require("lib/AnAL")
+require("lib.AnAL")
 
 local RC_Player = {}
 RC_Player.__index = RC_Player
+
+local img_walk = love.graphics.newImage("img/walk-sequence.png")
 
 function newPlayer(x, y)
   local new = {}
   new.x = x
   new.y = y
-  new.img = love.graphics.newImage("img/walk-sequence.png")
-  new.anim = newAnimation(new.img, 82, 119, 0.1, 0)
-  new.walk_direction = 'right'
+  new.anim_walk = newAnimation(img_walk, 82, 119, 0.1, 0)
+  new.img_stand = love.graphics.newImage("img/character-stand.png")
+  new.img_jump = love.graphics.newImage("img/character-jump.png")
+  new.look_direction = 'right'
+  new.move_action = 'stand'
 
   return setmetatable(new, RC_Player)
 end
 
 function RC_Player:move(x, y)
-  self.anim:play()
+  self.move_action = 'walk'
+  self.anim_walk:play()
   if x > 0 then
-    self.walk_direction = 'right'
+    self.look_direction = 'right'
   elseif x < 0 then
-    self.walk_direction = 'left'
+    self.look_direction = 'left'
   end
 
   self.x = self.x + x
   self.y = self.y + y
 end
 
-function RC_Player:update(dt)
-  self.anim:update(dt)
-  self.anim:stop()
+function RC_Player:jump()
 end
 
-function RC_Player:draw()
-  if self.walk_direction == 'right' then
-    self.anim:draw(self.x, self.y)
-  elseif self.walk_direction == 'left' then
-    self.anim:draw(self.x + 82, self.y, 0, -1, 1)
+
+function RC_Player:update(dt)
+  self.anim_walk:update(dt)
+  self.anim_walk:stop()
+  self.move_action = 'stand'
+end
+
+function RC_Player:draw()  
+  if self.move_action == 'walk' then    
+    if self.look_direction == 'right' then
+      self.anim_walk:draw(self.x, self.y)
+    elseif self.look_direction == 'left' then
+      self.anim_walk:draw(self.x + 82, self.y, 0, -1, 1)
+    end
+  elseif self.move_action == 'jump' then
+  else
+    if self.look_direction == 'right' then
+      love.graphics.draw(self.img_stand, self.x, self.y)
+    elseif self.look_direction == 'left' then
+      love.graphics.draw(self.img_stand, self.x + 82, self.y, 0, -1, 1)
+    end
   end
 end
